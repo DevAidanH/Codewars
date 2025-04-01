b9426962f06486ee4a92fa7ffc3510322408bdb6
DevAidan <aidanhumpidge333@gmail.com>
2025-04-01 16:11:01 +0100

    working

--- Python/justify.py
+++ Python/justify.py
@@ -32,58 +32,32 @@ nulla et dolor."""
 width = 30
 
 def justify(text, width):
-    #split string at each word and store in a list. Work through the list, 
-    #taking as many words as possible to match the width. filling 
-    #the rest with spaces and /n 
-    
-    x = text.split(" ")
-    currentLine = ""
-    output = []
-    buffer = ""
-    while len(x) > 0:
-        for i in x:
-            if buffer != "":
-                currentLine = currentLine + buffer
-            buffer = ""
-            if len(currentLine)+len(i)+1 < 30:
-                currentLine = currentLine+" "+i
-            else:
-                if len(currentLine) == 30:
-                    output.append(currentLine)
-                    currentLine = ""
-                    buffer = i
-                    x.pop(0)
-                elif len(currentLine)+len(i) > 30:
-                    j = 30 - len(currentLine)
-                    currentLine = disributeSpaces(currentLine, j)
-                    output.append(currentLine)
-                    currentLine = ""
-                    buffer = i
-                    x.pop(0)
-    print("\n".join(output))
-    return("\n".join(output))
-                    
-def disributeSpaces(text, spacesToAdd):
-    import re
+    words = text.split()[::-1]
+    buffer = [words.pop()]
+    result = []
 
-    # Find all spaces in the string
-    spaces = re.findall(" ", text)
+    while words:
+        word = words.pop()
+        if len(" ".join(buffer + [word])) > width:
+            result.append(justfiedJoin(buffer, width))
+            buffer = [word]
+        else:
+            buffer.append(word)
+    result.append(" ".join(buffer))
+    return "\n".join(result)
 
-    # Calculate how many extra spaces to add per existing space
-    extra_per_space = spacesToAdd // len(spaces)
-    remaining_spaces = spacesToAdd % len(spaces)
-
-    # Replace each space with the required number of extra spaces
-    new_text = text.replace(" ", " " * (extra_per_space + 1))
-
-    # If there's any remainder, add it to the first few spaces
-    if remaining_spaces:
-        new_text = new_text.replace(" " * (extra_per_space + 1), " " * (extra_per_space + 2), remaining_spaces)
-
-    return(new_text)
-
+def justfiedJoin(words, width): #Works out how many spaces need to be with each word to make the line
+    if len(words) == 1: return words[0]
+    nonspaces = sum(map(len,words))
+    spaces = [1] *(len(words)-1) + [0]
+    i = 0
+    while (sum(spaces) + nonspaces) < width:
+        spaces[i] += 1
+        i = (i+1) % (len(words)-1)
+    return "".join(f"{w}{s*" "}" for w,s in zip(words, spaces)) #zip function
     
 
 
-x = justify(" ".join(text.split()), 30)
-print(x==text)
+o = justify(" ".join(text.split()), 30)
+print(o)
+print(o==text)
