02a2eb34dc08dd0e5623f2c23ebb3478c23ba447
DevAidan <aidanhumpidge333@gmail.com>
2025-04-25 21:10:00 +0100

    window pattern study

--- Python/Python Technical Skills/window_pattern.py
+++ Python/Python Technical Skills/window_pattern.py
@@ -13,4 +13,18 @@ def max_sum_of_subarray(arr, k):
     return max_sum
 
 def max_average_in_array(arr, k):
-    pass
+    max_average = float('-inf')
+    window_average = 0
+    start = 0
+    window_sum = 0
+
+    for end in range(len(arr)):
+        window_sum += arr[end]
+        if end >= k - 1:
+            window_average = window_sum/k
+            max_average = max(max_average, window_average)
+            window_sum -= arr[start]
+            start += 1
+    return round(max_average,2)
+
+print(max_average_in_array([4, -1, 2, 1, -7, 5, 3, 6],3))
